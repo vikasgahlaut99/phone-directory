7c6de77af2f39a91f5e456b74490a5eabc38f446
vikasgahlaut99 <vikasgahlaut12@gmail.com>
2019-12-09 10:36:59 +0530

    State: Added state in ‘AddSubscriber’ and ‘App’ components

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import AddSubscriber from './AddSubscriber';
 import './common/common.css';
 
 
-ReactDOM.render(<AddSubscriber />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
